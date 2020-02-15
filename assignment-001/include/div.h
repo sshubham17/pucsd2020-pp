2924f112419cfd414dbb47442f9c7fc32553b410
Shubham Chincholkar <46499021+sshubham17@users.noreply.github.com>
2020-02-15 22:55:54 +0530

    Add files via upload

---
+++ assignment-001/include/div.h
@@ -0,0 +1,8 @@
+#include<stdlib.h>
+
+int division(char *a,char *b)
+{
+	int first_no=atoi(a);
+	int second_no=atoi(b);
+	return first_no / second_no;
+}
---
+++ assignment-001/include/sub.h
@@ -0,0 +1,8 @@
+#include<stdlib.h>
+
+int substraction(char *a,char *b)
+{
+	int first_no=atoi(a);
+	int second_no=atoi(b);
+	return first_no - second_no;
+}
